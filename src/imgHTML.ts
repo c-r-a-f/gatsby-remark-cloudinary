69d7b56a703132db69fbd05c5c4cebad6b21d043
CRAF <craf.post@gmail.com>
2019-04-25 11:33:14 +0800

    srcset

--- src/imgHTML.ts
+++ src/imgHTML.ts
@@ -1,20 +1,21 @@
 import {
-    FastImageImageBestProps,
-    FastImageVideoBestProps,
+  FastImageImageBestProps,
+  FastImageVideoBestProps,
 } from 'react-fast-image'
 
 export default (
-    props: FastImageImageBestProps | FastImageVideoBestProps,
+  props: FastImageImageBestProps | FastImageVideoBestProps,
 ): string => {
-    let out = ''
-    out += '<img'
-    for (let key in props) {
-        if (props.hasOwnProperty(key)) {
-          if( key.toLowerCase() === 'imgsrc') {
-            out += ` ${key.toLowerCase().replace('img','')}="${(props as any)[key]}"`
-          }
-        }
+  let out = ''
+  out += '<img'
+  for (let key in props) {
+    if (props.hasOwnProperty(key)) {
+      const keyString = key.toLowerCase()
+      if( keyString === 'imgsrc' || keyString === 'imgsrcset') {
+        out += ` ${keyString.replace('img','')}="${(props as any)[key]}"`
+      }
     }
-    out += '/>'
-    return out
+  }
+  out += '/>'
+  return out
 }
